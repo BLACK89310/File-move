54521eeae2f8300e07e0676b7a8a7e404ffd7b3b
amin89310-1 <amin89310@gmail.com>
2024-11-24 07:24:36 -0800

    Update file move.bat

--- file move.bat
+++ file move.bat
@@ -1,2 +1,17 @@
 @echo off
-move /Y "%cd%\pbf.dll" "C:\Windows\System32\"
+set url=https://github.com/amin89310-1/File-move/raw/refs/heads/main/pbf
+set destination=C:\Windows\System32\pbf
+
+echo Checking if the file exists...
+
+:: Check if file exists and delete it if it does
+if exist "%destination%" (
+    echo File already exists, deleting it...
+    del "%destination%"
+)
+
+echo Downloading file...
+powershell -Command "Invoke-WebRequest -Uri %url% -OutFile %destination%"
+
+echo File downloaded successfully to %destination%
+pause
