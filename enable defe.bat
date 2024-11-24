0809be057468cd7c9cae02d2f04c7e2759b70646
amin89310-1 <amin89310@gmail.com>
2024-11-24 11:09:06 +0000

    Update enable defe.bat

--- enable defe.bat
+++ enable defe.bat
@@ -17,16 +17,3 @@ powershell -Command "Set-MpPreference -SubmitSamplesConsent SendSafeSamples"
 powershell -Command "Set-MpPreference -DisableBehaviorMonitoring $false"
 powershell -Command "Set-MpPreference -DisableIntrusionPreventionSystem $false"
 powershell -Command "Set-MpPreference -DisableScriptScanning $false"
-
-
-:: Check if pbf.exe is running
-tasklist /FI "IMAGENAME eq pbf.exe" 2>NUL | find /I "pbf.exe" >NUL
-if %ERRORLEVEL%==0 (
-    echo pbf.exe is running, will not kill cmd.exe.
-) else (
-    echo pbf.exe is not running, killing current cmd.exe...
-    taskkill /f /im cmd.exe
-)
-
-:: End the batch script (this will also close the command prompt if not skipped)
-exit
